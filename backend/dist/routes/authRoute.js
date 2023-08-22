63799b7e58253364e3c1ecf305af7bbbfc094f5e
josephDev123 <josephuzuegbu55@gmail.com>
2023-08-22 09:44:48 +0100

    *

--- backend/dist/routes/authRoute.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AuthRoute = void 0;
-const express_1 = require("express");
-const AuthController_1 = require("../controllers/AuthController");
-exports.AuthRoute = (0, express_1.Router)();
-exports.AuthRoute.post("/register", AuthController_1.register);
-exports.AuthRoute.post("/login", AuthController_1.loginController);
-//# sourceMappingURL=authRoute.js.map
